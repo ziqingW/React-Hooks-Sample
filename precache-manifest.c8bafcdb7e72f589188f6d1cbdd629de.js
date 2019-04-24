bf9d4d9b4f2852cbdf87f1a4fcb98972c93b1cb7
redtaq <redtaq@hotmail.com>
2019-04-23 19:25:39 -0500

    Updates

---
+++ precache-manifest.c8bafcdb7e72f589188f6d1cbdd629de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89ddf70ec1ad46d816eeca91f4f20fbf",
+    "url": "/React-Hooks-Sample/index.html"
+  },
+  {
+    "revision": "a70f76e939247ea91d8a",
+    "url": "/React-Hooks-Sample/static/css/main.30571937.chunk.css"
+  },
+  {
+    "revision": "10bf244ca692441702db",
+    "url": "/React-Hooks-Sample/static/js/2.1b3a0643.chunk.js"
+  },
+  {
+    "revision": "a70f76e939247ea91d8a",
+    "url": "/React-Hooks-Sample/static/js/main.7c856430.chunk.js"
+  },
+  {
+    "revision": "d54af78cc9802bf4bdaa",
+    "url": "/React-Hooks-Sample/static/js/runtime~main.812f37eb.js"
+  }
+]);
