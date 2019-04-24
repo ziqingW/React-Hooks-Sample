6a9364c54898785a8fa418bc42409225fb26d027
redtaq <redtaq@hotmail.com>
2019-04-23 19:07:30 -0500

    first push

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
+  const initialItems = [
+    {
+    content: ['123',
+    '323d',
+    'juimrk'],
+    title: 'Great'
+  },
+    {
+    content: ['klkis',
+    'dsdaee',
+    'dsg4'],
+    title: 'Nice'
+  },
+    {
+    content: ['332',
+    '3dcc',
+    '7ujnf'],
+    title: 'Average'
+  },
+    {
+    content:['43f',
+    'kio',
+    'cvdws'],
+    title: 'OK'
+  }
+];
+  const inputDisplay = Array(initialItems.length).fill(false);
+  const [display, setDisplay] = useState(inputDisplay);
+  function addItem(i) {
+    setNewContent('');
+    let newDisplay = [...inputDisplay];
+    newDisplay[i] = true;
+    setDisplay(newDisplay);
+  }
+
+  const [newContent, setNewContent] = useState('');
+  function addInput(e) {
+    setNewContent(e.target.value);
+  }
+
+  const [items, setItems] = useState(initialItems);
+  function addContent(i) {
+    let newItems = JSON.parse(JSON.stringify(items));
+    if (newContent.trim().length) {
+      newItems[i].content.push(newContent);
+      setItems(newItems);
+      setNewContent('');
+    }
+    setDisplay(inputDisplay);
+  }
+
+  function delItem(i, j) {
+    let newItems = JSON.parse(JSON.stringify(items));
+    newItems[i].content.splice(j, 1);
+    setItems(newItems);
+  }
+  const titleColors = ['#f49841', '#7641f4', '#337add', '#179322'];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      {items.map((item, i) => {
+        return (
+          <div className="single-card" key={i}>
+            <h3 style={{backgroundColor: titleColors[i]}}>{item.title}</h3>
+            <ul>
+              {item.content.map((singleItem, j) => <li key={j}><button className="btn btn-del" onClick={() => delItem(i, j)}>-</button><p className="content">{singleItem}</p></li>)}
+            </ul>
+            {!display[i] ? (<button className="btn btn-add" onClick={() => addItem(i)}>+</button>) : null}
+            {display[i] ? (<div><textarea rows="4" onChange={e => addInput(e)}/><button className="btn-input" onClick={() => addContent(i)}>OK</button></div>) : null}
+        </div>)
+      })}
     </div>
   );
 }
-
-export default App;
